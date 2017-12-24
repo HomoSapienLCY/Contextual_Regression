d3db2891c5e212c28b18a931e4c705e0d937eb59
Liu Chengyu <liuchengyu@wustl.edu>
2017-12-23 23:51:51 -0800

    Add files via upload

---
+++ Contextual_Regression_Open_Chromatin/Ground_Truth_Test_Base/Testing_Peaks_Gen_Complex.py
@@ -0,0 +1,17 @@
+from Function import *
+
+total_data = int(sys.argv[1])
+noise_level = float(sys.argv[2])
+
+each_seq_size = seq_len
+
+generated_data, generated_weights = gen_data(each_seq_size, max_feature_signal, noise_level, total_data, weight_fun=sq_and_sqr)
+
+print "max value"
+print np.amax(generated_data)
+print "max weight"
+print np.amax(generated_weights)
+
+write_data(generated_data, "generated_data")
+write_data(generated_weights, "generated_weights")
+
